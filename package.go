b3a15282fb9c5a895e6ef7a5ed1a08ce15d0c564
samhit-bhogavalli <samhit.bhogavalli@razorpay.com>
2021-07-20 12:23:27 +0530

    packages in day 1

---
+++ package.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("a random number is ",rand.Intn(100))
+}
---
+++ variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var java , python bool
+
+func main(){
+	var temp int
+	fmt.Println(temp, java, python)
+}
