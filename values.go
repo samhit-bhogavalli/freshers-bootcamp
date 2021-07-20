d0647bd640e4128199eb96fef62db12ec221995e
samhit-bhogavalli <samhit.bhogavalli@razorpay.com>
2021-07-20 12:35:38 +0530

    done with variable and values

---
+++ values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("sum is", 45+59)
+	fmt.Println("string concat of go and lang is", "go"+"lang")
+	fmt.Println("division of floats", 7.0/3, 7.0/3.0, 7/3.0)
+	fmt.Println("testing and, or operations", true || false, true && false)
+}
--- variables.go
+++ variables.go
@@ -6,5 +6,9 @@ var java , python bool
 
 func main(){
 	var temp int
-	fmt.Println(temp, java, python)
+	var varcheck1 = true
+	varcheck2 := true
+	varcheck3, varcheck4 := 10, "string"
+	fmt.Println(temp, java, python, varcheck1, varcheck2)
+	fmt.Println(varcheck3, varcheck4)
 }
